dc2e0e6ab49a499f02d09b898811026e1d8bddac
autumn <156123199+itsautummn@users.noreply.github.com>
2025-02-02 22:10:18 -0800

    Update seq_align.py

--- seq_align.py
+++ seq_align.py
@@ -8,7 +8,7 @@ Steps:
 """
 def sequence_alignment(first: string, second: string):
     # Set up all variables
-    costMatrix = [[]]
+    costMatrix = []
 
     # Import all necessary files into proper data sets
     with open("imp2cost.txt", "r") as fcost:
@@ -16,32 +16,39 @@ def sequence_alignment(first: string, second: string):
         for line in csvCost:
             costMatrix.append(line)
     finput = open("imp2input.txt", "r")
-    for x in finput:
-        edit_dist(x.split(",")[0], x.split(",")[1], costMatrix)
-    print(costMatrix)
+    x = finput.readline()
+    print(f"Length 1 = {len(x.split(",")[0])}\nLength 2 = {len(x.split(",")[1])}")
+    minDist, distMatrix = edit_dist(x.split(",")[0], x.split(",")[1], costMatrix)
+    print(f"minDist = {minDist}")
+    for line in distMatrix:
+        print(line)
 
 
 def edit_dist(first: string, second: string, costMatrix: list[list]):
+    print(f"second[59] {second[59]}")
+    lenFirst = len(first)
+    lenSecond = len(second) - 1
+
     # Set up the 2D lists for later use (why is this so hard)
-    minLen = min(len(first), len(second))
-    distMatrix = [[None] * minLen] * minLen
-    distMatrix[0][0] = 0
-    ptr = [[None] * minLen] * minLen
+    print(lenSecond)
+    maxLen = max(lenFirst, lenSecond)
+    ptr = [[None] * maxLen] * maxLen
     ptr[0][0] = 0
 
-    for i in range(1, len(first)):
-        distMatrix[i][0] = i
-        ptr[i][0] = ptr[i - 1][0]
-    for j in range(1, len(second)):
-        distMatrix[0][j] = j
-        ptr[0][j] = ptr[0][j - 1]
+    distMatrix = []
+    for i in range(lenFirst):
+        distMatrix.append([i])
+        for j in range(1, lenSecond):
+            distMatrix[i].append(j)
     
-    for i in range(1, len(first)):
-        for j in range(1, len(second)):
+    for i in range(1, lenFirst):
+        print(f"Outer {i}")
+        for j in range(1, lenSecond):
+            print(f"Index {j}")
             distMatrix[i][j] = min(
                 distMatrix[i - 1][j] + 1,
                 distMatrix[i][j - 1] + 1,
-                distMatrix[i - 1][j - 1] + diff(first[i], second[j])
+                distMatrix[i - 1][j - 1] + diff(first[i], second[j], costMatrix)
             )
             match distMatrix[i][j]:
                 case 1:
@@ -51,14 +58,21 @@ def edit_dist(first: string, second: string, costMatrix: list[list]):
                 case 3:
                     ptr[i][j] = ptr[i - 1][j - 1]
     
-    return distMatrix[len(first)][len(second)], ptr
+    return distMatrix[lenFirst- 1][lenSecond - 1], distMatrix
 
 
-def diff(a, b) -> int:
-    if a == b:
-        return 0
-    if a != b:
-        return 1
+def diff(a, b, costMatrix) -> int:
+    fi = 0
+    si = 0
+    for i in range(len(costMatrix)):
+        if costMatrix[i][0] == a:
+            fi = i
+            break
+    for j in range(len(costMatrix)):
+        if costMatrix[0][j] == b:
+            si = j
+            break
+    return int(costMatrix[fi][si])
 
 
 def backtrace():
