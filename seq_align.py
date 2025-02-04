118951b46c9c451382e7afc3a9419681fdcbeb90
autumn <156123199+itsautummn@users.noreply.github.com>
2025-02-04 12:45:28 -0800

    Update seq_align.py
    This is close to the right answer, I think that the biggest issue is the cost being off. Once we figure out how to fix that, we can make sure the alignment is correct

--- seq_align.py
+++ seq_align.py
@@ -3,7 +3,7 @@ import csv
 import string
 
 
-def sequence_alignment(first: string, second: string):
+def sequence_alignment():
     # Import all necessary files into proper data sets
     costMatrix = []
     with open("imp2cost.txt", "r") as fcost:
@@ -11,15 +11,18 @@ def sequence_alignment(first: string, second: string):
         for line in csvCost:
             costMatrix.append(line)
     finput = open("imp2input.txt", "r")
-    x = finput.readline()
 
     # Perform the edit distance calculations
-    minDist, distMatrix = edit_dist(x.split(",")[0], x.split(",")[1], costMatrix)
+    for x in finput:
+        minDist, bt = edit_dist(x.split(",")[0], x.split(",")[1], costMatrix)
+        first, second = backtrace(bt, x.split(",")[0], x.split(",")[1])
+        print(f"{first},{second}:{minDist}")
 
-    # Print statements
-    print(f"minDist = {minDist}")
-    # for line in distMatrix:
-    #     print(line)
+    # # Perform the edit distance calculations
+    # x = finput.readline()
+    # minDist, bt = edit_dist(x.split(",")[0], x.split(",")[1], costMatrix)
+    # first, second = backtrace(bt, x.split(",")[0], x.split(",")[1])
+    # print(f"{first},{second}:{minDist}")
 
 
 def edit_dist(first: string, second: string, costMatrix: list[list]):
@@ -37,11 +40,12 @@ def edit_dist(first: string, second: string, costMatrix: list[list]):
             distMatrix[i].append(j)
     
     # Set up the 2D ptr list
-    ptr = []
-    for i in range(lenFirst):
-        ptr.append([None])
-        for j in range(1, lenSecond):
-            ptr[i].append(None)
+    # ptr = []
+    # for i in range(lenFirst):
+    #     ptr.append([i])
+    #     for j in range(1, lenSecond):
+    #         ptr[i].append(j)
+    # ptr = []
 
     # Fill in the 2D distMatrix list and the 2D ptr list
     # Largely similar to the psuedocode provided in lecture
@@ -52,20 +56,21 @@ def edit_dist(first: string, second: string, costMatrix: list[list]):
                 distMatrix[i][j - 1] + 1,
                 distMatrix[i - 1][j - 1] + diff(first[i], second[j], costMatrix)
             )
-            match distMatrix[i][j]:
-                case 1:
-                    ptr[i][j] = ptr[i - 1][j]
-                case 2:
-                    ptr[i][j] = ptr[i][j - 1]
-                case 3:
-                    ptr[i][j] = ptr[i - 1][j - 1]
-    
+            # ptr.append(distMatrix[i][j])
+            # # if distMatrix[i][j] == distMatrix[i - 1][j] + 1:
+            # #     ptr[i][j] = ptr[i - 1][j]
+            # # elif distMatrix[i][j] == distMatrix[i][j - 1] + 1:
+            # #     ptr[i][j] = ptr[i][j - 1]
+            # # else:
+            # #     ptr[i][j] = ptr[i - 1][j - 1]
+
     return distMatrix[lenFirst- 1][lenSecond - 1], distMatrix
 
 
 def diff(a, b, costMatrix) -> int:
     # Very crude and inefficient way of finding the cost of two characters via the cost matrix
-    x, y = 0
+    x = 0
+    y = 0
 
     for i in range(len(costMatrix)):
         if costMatrix[i][0] == a:
@@ -80,9 +85,48 @@ def diff(a, b, costMatrix) -> int:
     return int(costMatrix[x][y])
 
 
-def backtrace(): # Not implemented yet
-    pass
+def backtrace(bt, first, second): # Not implemented yet
+    lenFirst = len(bt)
+    lenSecond = len(bt[0])
+
+    lenAligned = max(lenFirst, lenSecond)
+
+    # Indecies
+    i = lenFirst - 1
+    j = lenSecond - 1
+
+    alignedFirst = ""
+    alignedSec = ""
+    while i > 0 and j > 0:
+        next = min(
+            bt[i - 1][j],
+            bt[i][j - 1],
+            bt[i - 1][j - 1]
+        )
+        if next == bt[i - 1][j]:
+            alignedFirst = first[i] + alignedFirst
+            alignedSec = "-" + alignedSec
+            i -= 1
+        elif next == bt[i][j - 1]:
+            alignedFirst = "-" + alignedFirst
+            alignedSec = second[j] + alignedSec
+            j -= 1
+        else:
+            alignedFirst = first[i] + alignedFirst
+            alignedSec = second[j] + alignedSec
+            i -= 1
+            j -= 1
     
+    while i > 0:
+        alignedFirst = first[i] + alignedFirst
+        alignedSec = "-" + alignedSec
+        i -= 1
+    while j > 0:
+        alignedFirst = "-" + alignedFirst
+        alignedSecond = second[j] + alignedSec
+        j -= 1
+    
+    return alignedFirst, alignedSec
 
 if __name__ == "__main__":
-    sequence_alignment("foo", "bar")
+    sequence_alignment()
