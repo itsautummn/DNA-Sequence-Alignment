c7b33aeca34e5085b419b6e48ebffbabca576794
autumn <156123199+itsautummn@users.noreply.github.com>
2025-02-04 13:01:28 -0800

    Update seq_align.py

--- seq_align.py
+++ seq_align.py
@@ -12,17 +12,17 @@ def sequence_alignment():
             costMatrix.append(line)
     finput = open("imp2input.txt", "r")
 
-    # Perform the edit distance calculations
-    for x in finput:
-        minDist, bt = edit_dist(x.split(",")[0], x.split(",")[1], costMatrix)
-        first, second = backtrace(bt, x.split(",")[0], x.split(",")[1])
-        print(f"{first},{second}:{minDist}")
-
     # # Perform the edit distance calculations
-    # x = finput.readline()
-    # minDist, bt = edit_dist(x.split(",")[0], x.split(",")[1], costMatrix)
-    # first, second = backtrace(bt, x.split(",")[0], x.split(",")[1])
-    # print(f"{first},{second}:{minDist}")
+    # for x in finput:
+    #     minDist, bt = edit_dist(x.split(",")[0], x.split(",")[1], costMatrix)
+    #     first, second = backtrace(bt, x.split(",")[0], x.split(",")[1])
+    #     print(f"{first},{second}:{minDist}")
+
+    # Perform the edit distance calculations
+    x = finput.readline()
+    minDist, bt = edit_dist(x.split(",")[0], x.split(",")[1], costMatrix)
+    first, second = backtrace(bt, x.split(",")[0], x.split(",")[1])
+    print(f"{first},{second}:{minDist}")
 
 
 def edit_dist(first: string, second: string, costMatrix: list[list]):
@@ -39,14 +39,6 @@ def edit_dist(first: string, second: string, costMatrix: list[list]):
         for j in range(1, lenSecond):
             distMatrix[i].append(j)
     
-    # Set up the 2D ptr list
-    # ptr = []
-    # for i in range(lenFirst):
-    #     ptr.append([i])
-    #     for j in range(1, lenSecond):
-    #         ptr[i].append(j)
-    # ptr = []
-
     # Fill in the 2D distMatrix list and the 2D ptr list
     # Largely similar to the psuedocode provided in lecture
     for i in range(1, lenFirst):
@@ -56,13 +48,6 @@ def edit_dist(first: string, second: string, costMatrix: list[list]):
                 distMatrix[i][j - 1] + 1,
                 distMatrix[i - 1][j - 1] + diff(first[i], second[j], costMatrix)
             )
-            # ptr.append(distMatrix[i][j])
-            # # if distMatrix[i][j] == distMatrix[i - 1][j] + 1:
-            # #     ptr[i][j] = ptr[i - 1][j]
-            # # elif distMatrix[i][j] == distMatrix[i][j - 1] + 1:
-            # #     ptr[i][j] = ptr[i][j - 1]
-            # # else:
-            # #     ptr[i][j] = ptr[i - 1][j - 1]
 
     return distMatrix[lenFirst- 1][lenSecond - 1], distMatrix
 
@@ -89,15 +74,13 @@ def backtrace(bt, first, second): # Not implemented yet
     lenFirst = len(bt)
     lenSecond = len(bt[0])
 
-    lenAligned = max(lenFirst, lenSecond)
-
     # Indecies
     i = lenFirst - 1
     j = lenSecond - 1
 
     alignedFirst = ""
     alignedSec = ""
-    while i > 0 and j > 0:
+    while i >= 0 and j >= 0:
         next = min(
             bt[i - 1][j],
             bt[i][j - 1],
@@ -117,15 +100,15 @@ def backtrace(bt, first, second): # Not implemented yet
             i -= 1
             j -= 1
     
-    while i > 0:
+    while i >= 0:
         alignedFirst = first[i] + alignedFirst
         alignedSec = "-" + alignedSec
         i -= 1
-    while j > 0:
+    while j >= 0:
         alignedFirst = "-" + alignedFirst
-        alignedSecond = second[j] + alignedSec
+        alignedSec = second[j] + alignedSec
         j -= 1
-    
+
     return alignedFirst, alignedSec
 
 if __name__ == "__main__":
